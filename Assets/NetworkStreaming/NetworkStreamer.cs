e1749e85ad3e5931614cf79ab244f863e81b5ac6
deadpanda1 <esberntk@gmail.com>
2016-12-02 10:45:25 +0100

    Streaming

---
+++ Assets/NetworkStreaming/NetworkStreamer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkStreamer : NetworkBehaviour {
+    public NetworkManager manager;
+    Vector3 rotations;
+    public GameObject Camera;
+	// Use this for initialization
+	void Start () {
+        //manager = GetComponent<NetworkManager>();
+        //manager.OnStartHost();
+        manager = GameObject.Find("Network Manager").GetComponent<NetworkManager>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (isServer)
+        {
+            Rpc_GetRotations();
+            transform.rotation = Quaternion.Euler(rotations);
+        }
+	}
+    void Update()
+    {
+        if (isClient && !isServer)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                transform.Rotate(new Vector3(10, 0, 0));
+            }
+            rotations = Quaternion.ToEulerAngles(transform.rotation);
+        }
+    }
+    //public virtual void OnStartHost()
+    //{
+    //    Camera = GameObject.Find("Main Camera");
+    //}
+    [Command]
+    public void Cmd_SendRotations(Vector3 clientRotate)
+    {
+        rotations = clientRotate;
+        Debug.Log("recieving stuff");
+    }
+    [ClientRpc]
+    public void Rpc_GetRotations()
+    {
+        if(isClient && !isServer)
+        Cmd_SendRotations(rotations);
+    }
+}
